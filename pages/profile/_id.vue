d4260a1c954a442b189beab3610fad4446d1a082
Avicenna <avcwisesa@gmail.com>
2018-06-28 15:55:22 +0700

    fix facet option ordering

--- pages/profile/_id.vue
+++ pages/profile/_id.vue
@@ -208,6 +208,14 @@ export default {
               code: obj['facet']['value'].split('/')[4],
               name: obj['facetLabel']['value']
             }
+          }).sort(function (a, b) {
+            if (a.name > b.name) {
+              return 1
+            } else if (a.name < b.name) {
+              return -1
+            } else {
+              return 0
+            }
           })
         })
         this.facetOptionsData = tmp
